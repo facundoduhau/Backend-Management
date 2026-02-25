a44a68429a58f6c3b9f32920676fd2a33cca89e7
facundoduhau <duhaufacundo@gmail.com>
2026-02-25 15:12:23 -0300

    Added api responses and modified app.js to inclued CORS

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ import cors from 'cors';
 const app = express();
 
 // Basics
-app.use(express.json({limit: '16kb'}));
-app.use(express.urlencoded({extended: true, limit: '16kb'}));
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 
 // CORS
 
+app.use(cors({
+	origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+	credentials: true,
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+	allowedHeaders: ["Authorization", "Content-Type"],
+}));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+	res.send('Hello World!')
 });
 
 export default app;
